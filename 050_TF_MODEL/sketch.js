be1a95361db8fb67c8e67ae80898cd9c2b0d9ce1
nikemr <nkemer@gmail.com>
2019-06-02 11:14:05 +0300

    hadi bakalım

--- 050_TF_MODEL/sketch.js
+++ 050_TF_MODEL/sketch.js
@@ -13,6 +13,11 @@ function brain() {
     const model = tf.model({ inputs: input, outputs: output });
 }
 var yon;
+var top;
+var bottom;
+var left;
+var right;
+
 function raider(params) {
     this.y = height / 2;
     this.x = 50
@@ -35,12 +40,41 @@ function raider(params) {
             this.x -= this.velocity;
            
         }
+        if (yon == 'sag') {
+            this.x += this.velocity;
+           
+        }
+        if (yon == 'yukarı') {
+            this.y -= this.velocity;
+           
+        }
+        if (yon == 'aşağı') {
+            this.y += this.velocity;
+           
+        }
+    }
+    this.think = function () {
+        let inputs = [];
+        inputs[0] = this.y / height;
+        inputs[1] = this.x / height;
+        inputs[2] = this.velocity / 10;
+        inputs[3] = top;
+        inputs[4] = bottom;
+        inputs[5] = left;
+        inputs[5] = right;
+        
+        let output = this.brain.predict(inputs);
+        //if (output[0] > output[1] && this.velocity >= 0) {
+        if (output[0] > output[1]) {
+            this.up();
+        }
+
     }
 
 }
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(1000, 1000);
     raider1 = new raider;
 
 }
@@ -49,23 +83,32 @@ function keyPressed() {
     if (key == '4') {
         console.log('sol');
         yon='sol'
-        raider1.velocity=10;       
+              
     }
     if (key == '8') {
         console.log('yukarı');
+        yon='yukarı'
     }
     if (key == '2') {
         console.log('aşağı');
+        yon='aşağı'
     }
     if (key == '6') {
         console.log('sağ');
+        yon='sag'
     }
     
 }
 function draw() {
-    
-    background(0);
 
+    background(0);
+    if (keyIsPressed === true) {
+        raider1.velocity=10; 
+    }
+    console.log( raider1.velocity);
+    if (raider1.velocity==0) {
+        yon=null; 
+    }
     raider1.update(yon);
     raider1.show();
     //raider1.update();
